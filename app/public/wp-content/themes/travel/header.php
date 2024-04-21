60c79aca1fedf6b2a12acf4ae40c46730fc61dc6
unknown <irena.sturua@gmail.com>
2024-04-21 19:45:34 +0400

    Add header and first section

--- app/public/wp-content/themes/travel/header.php
+++ app/public/wp-content/themes/travel/header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap">
 	<?php wp_head(); ?>
 </head>
 
@@ -25,35 +25,35 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'travel' ); ?></a>
 
-	<header id="masthead" class="site-header">
+	<header id="masthead" class="site-header travel-header">
+	<div class="container">
+		<div class="header-content">
 		<div class="site-branding">
+		
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$travel_description = get_bloginfo( 'description', 'display' );
-			if ( $travel_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $travel_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			?>
 		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
+		<div class="navigation-items">
+        <div class="navigation-travel">
+		<nav id="site-navigation" class="main-navigation travel-nav">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'travel' ); ?></button>
 			<?php
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
+					'menu_class'     => 'header-menu',
 				)
 			);
 			?>
 		</nav><!-- #site-navigation -->
+		</div>
+		<div class="search-icons">
+			<a href="" > <img src="<?php echo get_template_directory_uri() . '/assets/image/star.png'; ?>" alt=""></a>
+			<a href=""> <img src="<?php echo get_template_directory_uri() . '/assets/image/search.png'; ?>" alt=""></a>
+		</div>
+		</div>
+		</div>
+		</div>
 	</header><!-- #masthead -->
