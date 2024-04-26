f85efd3d189cf7009e1a3667076c941a397e0de6
unknown <irena.sturua@gmail.com>
2024-04-26 17:43:49 +0400

    responsive 5 sections

--- app/public/wp-content/themes/travel/front-page.php
+++ app/public/wp-content/themes/travel/front-page.php
@@ -16,17 +16,55 @@ $third_section = get_field('third_section');
 $map = isset($third_section['map']) ? $third_section['map'] : null;
 $third_section_title = $third_section['title'];
 $third_section_text = $third_section['text'];
-$best_services_id = $third_section['best_servises'];
+$best_services_id = $third_section['best_services'];
 // third section
 
 $green_button=get_field('fifth_section_buton');
+
+   // start seventh section
+
+
+$seventh_section_impressions_title = '';
+$seventh_section_impressions_first_cover = '';
+$seventh_section_impressions_second_cover = '';
+$seventh_section_impressions_third_cover = '';
+$impressions_covers = array();
+$impressions_id = '';
+
+if (function_exists('get_field') && get_field('seventh_section')) {
+    $seventh_section = get_field('seventh_section');
+
+    if (isset($seventh_section['impressions_title'])) {
+        $seventh_section_impressions_title = $seventh_section['impressions_title'];
+    }
+    if (isset($seventh_section['impressions_first_cover'])) {
+        $seventh_section_impressions_first_cover = $seventh_section['impressions_first_cover'];
+    }
+    if (isset($seventh_section['impressions_second_cover'])) {
+        $seventh_section_impressions_second_cover = $seventh_section['impressions_second_cover'];
+    }
+    if (isset($seventh_section['impressions_third_cover'])) {
+        $seventh_section_impressions_third_cover = $seventh_section['impressions_third_cover'];
+    }
+
+
+    $impressions_covers = array(
+        'impressions-first-cover' => $seventh_section_impressions_first_cover,
+        'impressions-second-cover' => $seventh_section_impressions_second_cover,
+        'impressions-third-cover' => $seventh_section_impressions_third_cover,
+    );
+
+
+    if (isset($seventh_section['impressions'])) {
+        $impressions_id = $seventh_section['impressions'];
+    }
+} 
+    // seventh section
 
 
 // echo '<pre>';
 // print_r($buttons);
 // echo '</pre>';
-
-
  get_header();
   ?>
 
@@ -265,12 +303,199 @@ endforeach;
    </div>
             <a href="<?php echo $contact_button['url']; ?>" class="button-green">Discover More</a>
 </section>
+<!-- Erekles Section -->
+<div class="tony_container">
+			<div class="book_trip_main">
+				<div class="book_trip_poster_front">
+								<?php
+									$trip_poster_card_next_book = get_field('trip_poster_card_next_book');
+								?>
+									<img src="<?php echo esc_url($trip_poster_card_next_book['url']); ?>" alt="">
+						</div>
+				<div class="book_trip_text">
 
+					<div class="book_trip_title">
+						<?php 
+							$book_trip_title = get_field('book_trip_title');
+						?>
+							<p>
+								<?php echo esc_html($book_trip_title); ?>
+                            </p>
+						
+					</div>
+					<div class="book_trip_text_text">
+						<?php 
+							$book_trip_text = get_field('book_trip_text');
+						?>
+							<p>
+								<?php echo esc_html($book_trip_text); ?>
+                            </p>
+					</div>
+				</div>
+				<div class="book_trip_cont">
+					<div class="book_trip_desc">
+						<?php
+							$trip_card = get_posts(array("post_type" => "trip_card"));
+							foreach ($trip_card as $trip_cardi) :
+								$trip_card_img = get_field('trip_card_img', $trip_cardi->ID);
+								$trip_card_title = get_field('trip_card_title', $trip_cardi->ID);
+								$trip_card_text = get_field('trip_card_text', $trip_cardi->ID);
+						?>
+						<div class="book_trip_desc_card">
+							<div class="book_trip_desc_card_img">
+								<img src="<?php echo esc_url($trip_card_img['url']); ?>" alt="">	
+							</div>
+							<div class="book_trip_desc_card_text">
+								<div class="book_trip_desc_card_text_title">
+									<p>
+										<?php echo esc_html($trip_card_title); ?>
+                            </p> 
+								</div>
+								<div class="book_trip_desc_card_text_text">
+									<p>
+										<?php echo esc_html($trip_card_text); ?>
+                            </p> 
+								</div>
+							</div>
+						</div>
+						<?php
+							endforeach;
+						?> 
+					</div>
+					<div class="book_trip_poster">
+						
+						<div class="book_trip_poster_backg">
+							<img class="book_back" src="<?php echo get_template_directory_uri() . "/assets/image/book_back_2.png" ?>" alt="">
+						</div>
+						<div class="book_trip_poster_card">
+							<div class="book_trip_poster_card_flex">
 
-				
+								<div class="book_trip_poster_card_img">
+									<?php
+									$trip_poster_card_img = get_field('trip_poster_card_img');
+									?>
+									<img src="<?php echo esc_url($trip_poster_card_img['url']); ?>" alt="">
 	
+								</div>
+								<div class="book_trip_poster_card_title">
+									<?php
+									$trip_poster_card_title = get_field('trip_poster_card_title');
+									?>
+										<p>
+											<?php echo esc_html($trip_poster_card_title); ?>
+                        </p> 
+								</div>
+								<div class="book_trip_poster_card_desc">
+									<?php
+										$trip_poster_card_desc = get_field('trip_poster_card_desc');
+									?>
+										<p>
+											<?php echo esc_html($trip_poster_card_desc); ?>
+                        </p> 
+								</div>
+								<div class="book_trip_poster_card_mini_images">
+									<?php
+										$trip_poster_card_mini_img_1 = get_field('trip_poster_card_mini_img_1');
+										$trip_poster_card_mini_img_2 = get_field('trip_poster_card_mini_img_2');
+										$trip_poster_card_mini_img_3 = get_field('trip_poster_card_mini_img_3');
+									?>
+									<img src="<?php echo esc_url($trip_poster_card_mini_img_1['url']); ?>" alt="">
+									<img src="<?php echo esc_url($trip_poster_card_mini_img_2['url']); ?>" alt="">
+									<img src="<?php echo esc_url($trip_poster_card_mini_img_3['url']); ?>" alt="">
+								</div>
+								<div class="book_trip_poster_card_booking">
+									<div class="book_trip_poster_card_booking_flex">
 
+										<img class="book_trip_poster_card_booking_building" src="<?php echo get_template_directory_uri() . "/assets/image/building.png" ?>" alt="">
+	
+										<?php
+											$trip_poster_card_book = get_field('trip_poster_card_book');
+										?>
+										<p>
+											<?php echo esc_html($trip_poster_card_book); ?>
+										<p>
+									</div>
 
+									<img class="book_trip_poster_card_booking_heart" src="<?php echo get_template_directory_uri() . "/assets/image/heart.png" ?>" alt="">
+
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	<!-- end erekles section -->
+
+	<!-- seventh section -->
+    <section class="home-section-7">
+    <div class="title">
+        <h2 class="seventh-title"><?php echo $seventh_section_impressions_title ?></h2>
+    </div>
+    <div class="cover-images">
+        <?php
+        foreach ($impressions_covers as $class => $impressions_cover_id) {
+            if ($impressions_cover_id) {
+                echo wp_get_attachment_image($impressions_cover_id, 'full', false, array('class' => $class));
+            }
+        }
+        ?>
+    </div>
+    <div class="container">
+        <div class="impressions-container">
+            <?php
+            if (!empty($impressions_id)) {
+                foreach ($impressions_id as $impression_id) {
+                    $impression_post = get_post($impression_id);
+                    if ($impression_post) {
+                        ?>
+                        <div class="all-impressions">
+                            <div class="impressions-img">
+                                <a href="<?php echo get_permalink($impression_post); ?>">
+                                    <?php echo get_the_post_thumbnail($impression_post, 'thumbnail'); ?>
+                                </a>
+                            </div>
+                            <div class="impressions-full-content">
+                                <p class="impressions-text"><?php echo get_the_excerpt($impression_post); ?></p>
+                                <h3 class="impressions-title"><?php echo get_the_title($impression_post); ?></h3>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                }
+            }
+            ?>
+        </div>
+    </div>
+</section>
+<!-- end of seventh section -->
+
+<!-- contact -->
+<div class="tony_container">
+	<!-- <div class="contact_position"> -->
+
+		<div class="contact-from">
+			<div class="contact-flex">
+				
+				<div class="contact-form">
+					
+					
+							<?php
+							echo do_shortcode('[contact-form-7 id="4b3126f" title="Contact form 1"]')
+							?>
+							<img class="arrow" src="<?php echo get_template_directory_uri() . "/assets/image/arrow.png" ?>" alt="">
+							<img class="up-lines" src="<?php echo get_template_directory_uri() . "/assets/image/up-lines.png" ?>" alt="">
+							<img class="down-lines" src="<?php echo get_template_directory_uri() . "/assets/image/down-lines.png" ?>" alt="">
+							<img class="pluses" src="<?php echo get_template_directory_uri() . "/assets/image/pluses.png" ?>" alt="">
+						
+
+				</div>
+			</div>
+		</div>
+		
+	<!-- </div> -->
+	</div>
 <?php
 
 get_footer();
